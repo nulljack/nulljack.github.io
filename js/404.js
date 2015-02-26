39c2706d9325c7c771f625086e39ab2d1e0d71fb
nulljack <nulljack@outlook.com>
2015-02-26 13:15:30 -0500

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -2,5 +2,5 @@ if (window.jQuery !== undefined) {
     $('#heading1').text('404');
     $('#heading2').text('- Page not found -');
     $('#heading1,#heading2').css('text-align', 'center');
-    $('.path').text(this.window.location.pathname);
+    $('.path').text('Path:'+'\t-\t'+this.window.location.pathname);
 }
