e70d1aa25b70feeb85287c0d2a4fb4c6122873c7
Nicholas Phillips <Nicophillips@live.com>
2015-09-14 08:21:36 -0400

    Cleaning out teh trash!

--- js/404.js
+++
@@ -1,6 +0,0 @@
-if (window.jQuery !== undefined) {
-    $('#heading1').text('404');
-    $('#heading2').text('- Page not found -');
-    $('#heading1,#heading2').css('text-align', 'center');
-    $('.path').text('Path:'+'\t\t'+this.window.location.pathname);
-}
